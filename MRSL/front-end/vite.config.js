8e110f23dd43b9cef4464accb7bbc08cb6b5bb9a
unknown <chandrotaypin@gmail.com>
2026-05-27 14:50:12 +0800

    fix: merge duplicate default exports in vite config

--- MRSL/front-end/vite.config.js
+++ MRSL/front-end/vite.config.js
@@ -4,9 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
-export default {
   server: {
     proxy: {
       '/admin': 'http://localhost:5001',
@@ -15,4 +12,4 @@ export default {
       '/transaction': 'http://localhost:5001',
     }
   }
-}
+})
